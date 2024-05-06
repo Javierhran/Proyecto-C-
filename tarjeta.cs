4257941b45d616ad23972e37ff1110faa1423c52
Javierhran <169016213+Javierhran@users.noreply.github.com>
2024-05-05 22:33:39 -0600

    Update tarjeta.cs

--- tarjeta.cs
+++ tarjeta.cs
@@ -5,7 +5,7 @@ public class TarjetaCredito
     public double Saldo { get; set; }
     public string Pin { get; set; }
     public bool Bloqueada { get; set; }
-    // Puedes agregar más propiedades según sea necesario
+   
 
     public TarjetaCredito(int numeroTarjeta, string titular, double saldo, string pin)
     {
